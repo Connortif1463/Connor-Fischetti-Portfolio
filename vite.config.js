c0099d14f2f29815751bbe19297f4e3fd111c24a
ctfis <fischet9@msu.edu>
2026-02-10 15:07:24 -0500

    fixing vite, gitignore etc.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Connor-Fischetti-Portfolio/'
+  base: '/Connor-Fischetti-Portfolio/',
+  emptyOutDir: true,
 })
