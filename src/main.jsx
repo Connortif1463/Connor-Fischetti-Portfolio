5cf59eba818dd35c982bd4aa3bc79fb8baa5d529
ctfis <fischet9@msu.edu>
2026-02-11 00:15:15 -0500

    fix viewport bug

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
 import theme from './theme.js'
 
+// html doesn't save viewport, so this fixes it
+if (!document.querySelector('meta[name="viewport"]')) {
+  const meta = document.createElement('meta');
+  meta.name = 'viewport';
+  meta.content = 'width=device-width, initial-scale=1.0, minimum-scale=1.0';
+  document.head.prepend(meta);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
